e18fe35b7bfec19f5cdeb5693cebfe4c5426503e
Niall Palfreyman <80033589+NiallPalfreyman@users.noreply.github.com>
2021-12-05 18:54:08 +0100

    Documented cheers()

--- src/Ingolstadt.jl
+++ src/Ingolstadt.jl
@@ -2,6 +2,25 @@ module Ingolstadt
 
 export cheers
 
+"""
+    cheers(n)
+
+Add 2 to the number of cheers.
+
+A more detailed explanation can go here.
+
+# Arguments
+* `n`: The number to which we add 2
+
+# Notes
+* Notes can go here
+
+# Examples
+```julia
+julia> five = plusTwo(3)
+5
+```
+"""
 function cheers( n::Int)
 	c = n+2
 	println( c, " cheers for You!!")
