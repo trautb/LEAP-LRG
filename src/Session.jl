79a50de80c37e1a37c14ba69372e24b6ee17fc09
Niall Palfreyman <80033589+NiallPalfreyman@users.noreply.github.com>
2022-03-18 11:27:57 +0100

    Hints no longer offered automatically

--- src/Session.jl
+++ src/Session.jl
@@ -127,11 +127,7 @@ commiserate( act::Activity)
 Provide supportive feedback to an unsuccessful learner response.
 """
 function commiserate( act::Activity)
-	print( "Not quite right, I'm afraid. Would you like to see a hint? ")
-	if occursin( "yes", lowercase(readline()))
-		# Learner requires a hint as to how to respond to the activity:
-		hint( act)
-	end
+	print( "Not quite right, I'm afraid. You may like to ask for a hint()")
 end
 
 #-----------------------------------------------------------------------------------------
