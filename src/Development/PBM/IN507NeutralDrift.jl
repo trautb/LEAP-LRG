389126a061af5089ff756ef9abe252ea3538da97
nick.diercksen <nick.diercksen@student.hswt.de>
2022-05-31 01:25:04 +0100

    first working version of 507NeutralDrift

---
+++ src/Development/PBM/IN507NeutralDrift.jl
@@ -0,0 +1,146 @@
+#========================================================================================#
+"""
+	IN507NeutralDrift
+
+**Module NeturalDrift**: work in progress. \\
+    This model runs a very simple simulation called a Moran process. At each
+    step, a random individual dies, and is replaced by a child of one of its
+    neighbours. We have added in here the possibility that blue individuals have
+    a slight selective advantage over green individuals, but notice how even
+    such a tiny advantage a huge effect on the outcome of the simulation!
+
+
+Author: , 30/05/22
+"""
+module NeutralDrift
+
+export demo_explorer                # Externally available names
+using Agents, GLMakie, InteractiveDynamics   # Required packages
+
+#-----------------------------------------------------------------------------------------
+# Module definitions:
+
+#-----------------------------------------------------------------------------------------
+"""
+	Patch
+
+To construct a new agent implementation, use the @agent macro and add all necessary extra
+attributes. Again, id, pos and vel are automatically inserted.
+"""
+@agent Patch GridAgent{2} begin end
+
+const blue = true;
+const lime = false;
+
+#-----------------------------------------------------------------------------------------
+# Module methods:
+
+#-----------------------------------------------------------------------------------------
+"""
+    initialize_model()
+
+Create the world model.
+"""
+function initialize_model(;
+    pB=0.5,                 # proportion of blues
+    AB=0.000,                 # Evolutionary advantage of blue individuals
+    worldsize=32,
+    extent=(worldsize, worldsize)
+)
+
+    properties = Dict(
+        :patches => falses(extent),
+        :pB => pB,
+        :AB => AB,
+        :spu => 30,
+        :sleep => 0.0,
+        :initialized => false)
+
+    model = ABM(Patch, GridSpace(extent); properties=properties)
+    add_agent!(model) # Add one dummy agent so that abm_video will allow us to plot.
+    return model
+end
+
+"""
+    spawn_blues!()
+
+Spawn blue patches according to the proportion of blues `pB`.
+"""
+function spawn_blues!(model)
+    model.patches = rand(size(model.patches)...) .< model.pB
+    return model
+end
+
+getAgentsByType(model, type) = [particle for particle in allagents(model) if particle.type == type]
+
+
+#-----------------------------------------------------------------------------------------
+"""
+    model_step!(world)
+
+Simulate a Moran process, in which a random individual dies, and is replaced by a
+child from a random neighbour, except that blues may have an evolutionary advantage AB.
+"""
+function model_step!(model)
+    if !model.initialized
+        spawn_blues!(model)
+        model.initialized = true
+    end
+
+    patch = rand(CartesianIndices(model.patches))
+    random_neighbour = patch + rand(nearby_positions(patch.I, model).itr.iter)
+    random_neighbour = min(
+        CartesianIndex(size(model.space)...),
+        max(CartesianIndex(1, 1), random_neighbour)
+    )
+    model.patches[patch] = model.patches[random_neighbour]
+
+    # If that colour was blue, do it, otherwise only set
+    # to green with a certain (unfair) probability:
+    keeping_it_fair = model.patches[patch] == lime && rand() < 1 - model.AB
+    model.patches[patch] = keeping_it_fair ? lime : blue
+    return model
+end
+
+#-----------------------------------------------------------------------------------------
+# function demo()
+#     model = initialize_model()# Create a EmergentBehaviour world ...
+
+#     abmvideo(# ... then make a pretty video of it.
+#         "03 Emergent Behaviour.mp4", model, agent_step!, model_step!;
+#         ac=:red, am=particle_marker, as=10,
+#         framerate=20,
+#         frames=100,
+#         title="03 Emergent Behaviour"
+#     )
+# end
+
+"""
+    demo_explorer()
+
+    increase the `spu` slider to see faster progress
+    Currently very laggy since creating agents and not plotting
+"""
+function demo_explorer()
+    params = Dict(
+        :pB => 0:0.1:1,
+        :AB => 0:0.001:0.01
+    )
+
+    plotkwargs = (
+        add_colorbar=false,
+        heatarray=:patches,
+        heatkwargs=(
+            colorrange=(0, 1),
+            colormap=cgrad([:lime, :blue]; categorical=true),
+        ),
+        as = 0,
+        title = "Nuetral Drift:",
+    )
+
+    model = initialize_model()
+    fig, p = abmexploration(model; (agent_step!)=dummystep, model_step!, params, plotkwargs...)
+    fig
+end
+
+end # ... of module EmergentBehaviour
