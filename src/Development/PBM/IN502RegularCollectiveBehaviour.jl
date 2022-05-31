9ac86a0c39922040ccee48762e3f9eec54bb2fe7
Stefan <stefan-hausner96@hotmail.de>
2022-05-31 17:10:09 +0200

    Lab02 Update

--- src/Development/PBM/IN502RegularCollectiveBehaviour.jl
+++ src/Development/PBM/IN502RegularCollectiveBehaviour.jl
@@ -24,7 +24,16 @@ end
 function initialize_model(;n_particles = 20,mdist = 0.0,worldsize,psize,griddims = (worldsize, worldsize),particle_speed)
 space2d = ContinuousSpace(griddims, 1.0)
 
-model = ABM(Particle,space2d, scheduler = Schedulers.randomly,properties = properties = (patches = zeros(Int, griddims), globaldist = zeros(n_particles,2), meadist = 0.0))
+
+properties = (
+    globaldist = zeros(n_particles,2) ,
+    tick = 0,
+    meadist =  0.0
+    )
+#properties["globaldist"] = zeros(n_particles,2)
+#properties["meadist"] = 0.0
+
+model = ABM(Particle,space2d, scheduler = Schedulers.randomly,properties = properties)
 
 
 #for I in CartesianIndices(model.patches)
@@ -54,6 +63,7 @@ for _ in 1:n_particles
     counter += 1
 end
     globaldist = dist
+    meadist = 0.0
     return model
 end
 
@@ -69,18 +79,31 @@ function agent_step!(particle,model)
         particle.rot = rand()*((1/36)*π);
         particle.vel= rotate_2dvector(particle.rot,particle.vel)
         move_agent!(particle,model,particle.speed);
-        globaldist[particle.id,:] = particle.pos .- Tuple([worldsize/2 worldsize/2]')
-        particle.mdist = mean(globaldist,dims=1);
+        #model.globaldist[particle.id,:] = particle.pos .- Tuple([worldsize/2 worldsize/2]')
+        #particle.mdist = mean(globaldist,dims=1);
+        #model.meadist = mean(globaldist,dims=1)[1];
     end
     
 end
+
+function model_step!(model)
+    for p in positions(model)
+        model.meadist = mean(model.globaldist,dims=1)[1];
+    end
+    model.tick += 1
+end
+
 function demo(world_size,particle_size,particle_speed)
     model = initialize_model(worldsize = world_size,psize=particle_size,particle_speed=particle_speed);
-    #println(model.pos)
+    #rintln(:maedist)
     #dist = mean(model.globaldist,dims=1)
-    maedist = mean(model.globaldist,dims=1)[1];
-    adata = [:rot]
+    #println(model.maedist)
+    #-----------------------
     
+    globaldist(model) = mean(model.globaldist,dims=1)[1]
+    mdata = [globaldist, :meadist]
+    
+    #-----------------------
     #plotkwargs = (
     #heatarray = :patches,
 
@@ -90,9 +113,20 @@ function demo(world_size,particle_size,particle_speed)
     #),
     
     #)
-    fig, p = abmexploration(model;agent_step!,params = Dict(),am = particle_marker, adata= adata, alabels  = ["plot"])#,plotkwargs...);
+
+    p = lines!( model_step!, model.globaldist, color = :red)
+    agent_df, model_df =run!(model, agent_step!, model_step!, 1000; mdata = mdata)
+    figure, p = abmexploration(model;agent_step!,params = Dict(),am = particle_marker)#,plotkwargs...);
     
-    fig;
+    #ax1 = figure[1, 1] = Axis(p, ylabel = "main_plot", textsize = 12)
+    #ax3 = p[1, 2] = Axis(p, ylabel = "temperature", textsize = 12)
+    
+    #lines!(ax3, model_step!, model.meadist, color = :red)
+    #lines!(ax3, model_df[!, :step], model_df[!, :maedist], color = :red)
+    figure;
+
+    #fig, p = abmexploration(model;agent_step!,model_step!,params = Dict(),am = particle_marker)#, mdata= mdata, mlabels  = ["plot","p"])#,plotkwargs...);
+    
 end
 
 end
