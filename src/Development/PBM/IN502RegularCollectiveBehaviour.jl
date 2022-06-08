24bc7d2c746844ebb0ea9e568c5a80785c1b8a85
Stefan <stefan.hausner@student.hswt.de>
2022-06-08 12:06:48 +0200

    Swarm Interference

--- src/Development/PBM/IN502RegularCollectiveBehaviour.jl
+++ src/Development/PBM/IN502RegularCollectiveBehaviour.jl
@@ -1,3 +1,4 @@
+
 module CollectiveBehaviour
 using Agents
 using InteractiveDynamics, GLMakie,LinearAlgebra, Random, Statistics
@@ -21,12 +22,26 @@ function rotate_2dvector(rotation,vector)
     end
 end
 
-function initialize_model(;n_particles = 20,mdist = 0.0,worldsize,psize,griddims = (worldsize, worldsize),particle_speed)
+function initialize_model(;n_particles = 20,globaldist = zeros(n_particles,2),tick=0,meadist=0,worldsize,psize,griddims = (worldsize, worldsize),particle_speed)
 space2d = ContinuousSpace(griddims, 1.0)
+#=
+properties = Dict(
+    globaldist => globaldist,
+    tick => tick,
+    meadist => meadist
+)
+=#
+properties = Dict(
+    globaldist => globaldist,
+    tick => tick,
+    meadist => meadist
+)
+properties[:globaldist] = zeros(n_particles,2)
+properties[:tick] = 0
+properties[:meadist] = 0.0
 
-model = ABM(Particle,space2d, scheduler = Schedulers.randomly,properties = properties = (patches = zeros(Int, griddims), globaldist = zeros(n_particles,2), meadist = 0.0))
-
-
+#zeros(n_particles,2);
+model = ABM(Particle,space2d, scheduler = Schedulers.randomly,properties = properties)
 #for I in CartesianIndices(model.patches)
 #    model.patches[I] =  rand(0:1)
 #end
@@ -53,7 +68,8 @@ for _ in 1:n_particles
     )
     counter += 1
 end
-    globaldist = dist
+    model.globaldist = dist
+    model.meadist = mean(dist,dims=1)[1];
     return model
 end
 
@@ -69,18 +85,31 @@ function agent_step!(particle,model)
         particle.rot = rand()*((1/36)*π);
         particle.vel= rotate_2dvector(particle.rot,particle.vel)
         move_agent!(particle,model,particle.speed);
-        globaldist[particle.id,:] = particle.pos .- Tuple([worldsize/2 worldsize/2]')
-        particle.mdist = mean(globaldist,dims=1);
+        dist = particle.pos .- Tuple([100/2 100/2])
+        model.globaldist[particle.id,:] = [dist[1] dist[2]]' #ändern
     end
     
 end
+
+function model_step!(model)
+    model.meadist = mean(model.globaldist,dims=1)[1];
+    model.tick += 1
+end
+
 function demo(world_size,particle_size,particle_speed)
     model = initialize_model(worldsize = world_size,psize=particle_size,particle_speed=particle_speed);
-    #println(model.pos)
-    #dist = mean(model.globaldist,dims=1)
-    maedist = mean(model.globaldist,dims=1)[1];
-    adata = [:rot]
     
+    #rintln(:maedist)
+    #dist = mean(model.globaldist,dims=1)
+    #println(model.maedist)
+    #-----------------------
+    println("her")
+
+    println(model.meadist)
+    #:meadist = rand(0:10)
+    mdata = [:meadist]
+
+    #-----------------------
     #plotkwargs = (
     #heatarray = :patches,
 
@@ -90,9 +119,44 @@ function demo(world_size,particle_size,particle_speed)
     #),
     
     #)
-    fig, p = abmexploration(model;agent_step!,params = Dict(),am = particle_marker, adata= adata, alabels  = ["plot"])#,plotkwargs...);
+
+    #p = lines!( model_step!, model.globaldist, color = :red)
+    #agent_df, model_df =run!(model, agent_step!, model_step!, 1000; mdata = mdata)
+    #agent_df, model_df =run!(model, agent_step!, model_step!, model.tick; mdata = mdata)
+    figure,p= abmexploration(model;model_step!,agent_step!,params = Dict(),am = particle_marker,mdata)#,plotkwargs...);
+    plot_layout = figure[:,end+1] = GridLayout()
+    count_layout = plot_layout[1,1] = GridLayout()
+    ax_counts = Axis(count_layout[1,1];
+    backgroundcolor = :white, ylabel = "Meandist")
+    #mea = @lift(Point2f.($(p.mdf).tick, $(p.mdf).meadist))
+    #mea = @lift(Point2f.($(p.mdf).step, $(p.mdf).meadist))
+    mea = @lift(Point2f.($(p.mdf).step, $(p.mdf).meadist))
+    println("------------")
+    agent_step = @lift($(p.mdf).step)
+    agent_steps = to_value(agent_step)
+    println(agent_steps[1])
+
+    scatterlines!(ax_counts,mea)
     
-    fig;
+    on(p.model) do m
+        autolimits!(ax_counts)
+    end
+    step!(p,1);
+    
+
+   
+    
+    #figure[1, 3] = Axis(figure, ylabel = "main", textsize = 12)
+    #ax1 = figure[1, 1] = Axis(p, ylabel = "main_plot", textsize = 12)
+    #ax3 = p[1, 2] = Axis(p, ylabel = "temperature", textsize = 12)
+    #xs = 0:0.01:12
+    #ys = 0.5 .* sin.(xs)
+    #lines!(figure[1, 3],mdata[!, :step], mdata[!, :meadist])
+    #lines!(ax3, model_df[!, :step], model_df[!, :maedist], color = :red)
+    figure;
+
+    #fig, p = abmexploration(model;agent_step!,model_step!,params = Dict(),am = particle_marker)#, mdata= mdata, mlabels  = ["plot","p"])#,plotkwargs...);
+    
 end
 
 end
