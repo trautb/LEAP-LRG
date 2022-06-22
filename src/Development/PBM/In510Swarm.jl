891660f043fdb751d9e43137496e22a080eba661
Stefan <stefan-hausner96@hotmail.de>
2022-06-22 10:57:49 +0200

    swarm update

--- src/Development/PBM/In510Swarm.jl
+++ src/Development/PBM/In510Swarm.jl
@@ -21,6 +21,8 @@ export demo
         )
         space2d = ContinuousSpace(griddims, 1.0)
 
+        patches = deJong7 ? buildDeJong7(worldsize) : buildValleys(worldsize)
+
         properties = Dict(
             :patches => patches,
             :pPop => pPop,
@@ -28,39 +30,12 @@ export demo
             :deJong7 => deJong7
         )
 
-        ms = MyScheduler(1)
+        
         model = ABM(Agent, space2d, scheduler = Schedulers.fastest,properties = properties)
-        if deJong7 == true
-            xy = ((collect(-(worldsize/2):1:(worldsize/2))).*10)./worldsize
-            for i = 1:worldsize
-                x = xy[i]
-                for j = 1:worldsize
-                    y = xy[j]
-                    patches[i,j] = x * sin(180 * 4 * x / pi) + 1.1 * y * sin(180 * 2 * y / pi);
-                    
-                end
-            end
-        else
-            xy = ((collect(-(worldsize/2):1:(worldsize/2))).*6)./worldsize
-            #patches = map(((xy,xy),) -> (1/3)*exp(-((x+1)^2)-(y^2))+10*(x/5-(x^3)-(y^5))*exp(-(x^2)-(y^2))-(3*((1-x)^2))*exp(-(x^2)-((y+1)^2)),xy,xy);
-            
-            for i = 1:worldsize
-                y = xy[i]
-                for j = 1:worldsize
-                    x = xy[j]
-                    patches[i,j] = (1/3)*exp(-((x+1)^2)-(y^2))+10*(x/5-(x^3)-(y^5))*exp(-(x^2)-(y^2))-(3*((1-x)^2))*exp(-(x^2)-((y+1)^2));
-                    
-                end
-            end
-            
-        end
         
         
-        
-
-        #world.patches = world.patches .- world.patches'
         for _ in 1:n_sources
-            pos_random = rand(10:1:150,2,1)
+            pos_random = rand(2:1:78,2,1)
             pos = Tuple([pos_random[1] pos_random[2]])
             patchvalue = model.patches[round(Int,pos[1]),round(Int,pos[2])]
             vel = Tuple([1 1])
@@ -75,8 +50,23 @@ export demo
         return model
     end
 
+    function buildValleys(worldsize)
+        maxCoordinate = worldsize / 2
+        xy = 4 .* collect(-maxCoordinate:(maxCoordinate-1)) ./ maxCoordinate
+        f(x, y) = (1 / 3) * exp(-((x + 1)^2) - (y^2)) +
+                  10 * (x / 5 - (x^3) - (y^5)) * exp(-(x^2) - (y^2)) -
+                  (3 * ((1 - x)^2)) * exp(-(x^2) - ((y + 1)^2))
+        f.(xy, xy')
+    end
+    function buildDeJong7(worldsize)
+        maxCoordinate = worldsize / 2
+        xy = 20 .* collect(-maxCoordinate:(maxCoordinate-1)) ./ maxCoordinate
+        f(x, y) = sin(180 * 2 * x / pi) / (1 + abs(x)) + sin(180 * 2 * y / pi) / (1 + abs(y))
+        f.(xy, xy')
+    end
+
     function agent_step!(sources,model)
-        ids = collect(nearby_ids(sources.pos, model, 8,exact=false))
+        ids = collect(nearby_ids(sources.pos, model, 12,exact=false))
         min_patchvalue = sources.patchvalue
         best_pos = sources.pos
         for id in ids
@@ -95,7 +85,7 @@ export demo
         end
         
         
-        
+        #=
         if sources.vel[1]<0 && sources.vel[2] < 0
             sources.vel = sources.vel .+ (Tuple([-1 -1]).-sources.vel)
         elseif sources.vel[1]>0 && sources.vel[2] > 0
@@ -105,6 +95,7 @@ export demo
         elseif sources.vel[1]>0 && sources.vel[2] < 0
             sources.vel = sources.vel .+ Tuple([(1-sources.vel[1]) (-1-sources.vel[2])])
         end
+        =#
         sources.patchvalue = model.patches[round(Int,sources.pos[1]),round(Int,sources.pos[2])]
         move_agent!(sources,model,1);
     end
@@ -117,21 +108,24 @@ export demo
     end
 
     function demo()
-        model = initialize_model(worldsize = 160);
-        heatarray = :patches
-        heatkwargs = (colorrange = (-8,0),colormap = cgrad(:ice))
+        model = initialize_model(worldsize = 80);
 
-        plotkwargs = (;
-        heatarray,
-        add_colorbar = false,
-        heatkwargs,
-        
-        );
+                  #
+
+        plotkwargs = (
+            add_colorbar=false,
+            heatarray=:patches,
+            heatkwargs=(
+                # colorrange=(-8, 0),
+                colormap=cgrad(:ice),
+                ),
+                
+            )
         
         #https://makie.juliaplots.org/stable/documentation/figure/
         #https://makie.juliaplots.org/v0.15.2/examples/layoutables/gridlayout/
         figure,p= abmexploration(model;agent_step!,params = Dict(),am = particle_marker,ac = :red,plotkwargs...)
-        Colorbar(figure[:,end+1],colormap = cgrad(:ice),limits = (-8,0))
+        #Colorbar(figure[:,end+1],colormap = cgrad(:ice),limits = (-8,0))
         figure 
     end
     
