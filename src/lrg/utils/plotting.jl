17bbde2985c8dba13d8db72b6af1e8d05c3f7560
Michael Staab <michael.staab@student.hswt.de>
2022-06-26 17:38:14 +0200

    document code in plotting.jl properly

--- src/lrg/utils/plotting.jl
+++ src/lrg/utils/plotting.jl
@@ -1,9 +1,16 @@
+# =========================================================================================
+### plotting.jl:
+#	Provides various functions to visualize simulation results using the Plots.jl package
+# =========================================================================================
+
 """
-Set specific parameters for the given plot.
+	finalizePlot!(p::Plots.Plot)
+
+Set specific parameters for the given plot to make the output prettier.
 """
 function finalizePlot!(p::Plots.Plot)
 	plot!(p,
-		size = (1920, 1080), # Full-HD
+		size = (1920, 1080), 			# Full-HD
 		bottom_margin = 10Plots.mm,
 		left_margin = 15Plots.mm,
 	)
@@ -11,11 +18,17 @@ function finalizePlot!(p::Plots.Plot)
 	return p
 end
 
+# -----------------------------------------------------------------------------------------
 """
 	compareMinimumScores(simulationData::Dict{GASimulation, DataFrame})
 
 This function takes the results of various simulations and returns a plot, that compares the 
-minimal scores per step for each simulation result. 
+minimal scores per number of modifications for each simulation result. 
+
+Attention: The DataFrames in `simulationData` have to be structured like the output of 
+`processSimulationData(agentDF::DataFrame)` in `save.jl`.
+
+Returns the finalized plot (see also `finalizePlot!`)
 """
 function compareMinimumScores(simulationData::Dict{GASimulation, DataFrame})
 	# Initialize plot:
@@ -37,6 +50,7 @@ function compareMinimumScores(simulationData::Dict{GASimulation, DataFrame})
 	ylabel!("Maximally Epistatic Function (Mepi)")
 	title!("Minimum mepi-values for different algorithms over time")
 
+	# Return the finalized plot:
 	return finalizePlot!(minimumComparison)
 end
 
@@ -44,26 +58,39 @@ end
 """
 	scoreOverTime(agentDF::DataFrame, algorithm::GeneticAlgorithm)
 
-This function takes an agent dataframe and plots the score per step for every agent.
+This function takes the data of a simulation (``agentDF``) and plots the score per step for every 
+organism.
+
+Attention: This function is very expensive for a large number of organisms and steps!
+
+Returns the finalized plot (see also `finalizePlot!`).
 """
 function scoreOverTime(agentDF::DataFrame, algorithm::GeneticAlgorithm)
+	# Transform the simulation data to get score of each organism per step:
 	pltDF = unstack(agentDF, :step, :organism, :score)
 
+	# Create the plot. The plot contains a seperate line for each organism:
 	plt = Plots.plot(
 		Matrix(pltDF[:, Symbol.(unique!(sort!(agentDF[:, :organism])))]),  # Select organisms only
 		legend = false, 
 		title = repr(algorithm)
 	)
 
+	# Return the finalized plot:
 	return finalizePlot!(plt)
 end
 
 # -----------------------------------------------------------------------------------------
 """
-	scoreSpanOverTime(modelDF::DataFrame, algorithm::GeneticAlgorithm)
+	scoreSpanOverTime(processedDF::DataFrame, algorithm::GeneticAlgorithm)
 
-This function takes an model dataframe and the maximum, minimum and mean score for every simulation 
-step.
+This function takes a DataFrame of processed simulation data (``processedDF``) and plots and the 
+maximum, minimum and mean score over time.
+
+Attention: `processedDF` has to be a DataFrame structured like the output of 
+`processSimulationData(agentDF::DataFrame)` in `save.jl`.
+
+Returns the finalized plot (see also `finalizePlot!`).
 """
 function scoreSpanOverTime(processedDF::DataFrame, algorithm::GeneticAlgorithm)
 	plt = Plots.plot(
@@ -76,6 +103,7 @@ function scoreSpanOverTime(processedDF::DataFrame, algorithm::GeneticAlgorithm)
 		title = repr(algorithm)
 	)
 
+	# Return the finalized plot:
 	return finalizePlot!(plt)
 end
 
@@ -83,13 +111,27 @@ end
 """
 	topTierOverTime(
 		agentDF::DataFrame, 
-		modelDF::DataFrame, 
 		percentage::Number, 
-		algorithm::GeneticAlgorithm
+		algorithm::GeneticAlgorithm;
+		maxBins::Integer = 100
 	)
 
-This function takes an agent dataframe and plots number of agents in range percentage around the 
-max score at that point of time.
+This function takes the data of a simulation (``agentDF``) and plots the number of organisms in 
+range ``percentage`` around the best score ("top-tiers") for each number of modifications. If the 
+number of steps taken from ``agentDF`` is greater than ``maxBins``, the data has to be processed 
+before plotting:
+
+--> If the number of simulation steps is to high, the bar chart would become a black block, so 	
+	the number of bins is restricted to ``maxBins``  
+--> To keep the visualization consistent with the message of the data, each bin represents the 
+	MEAN of the number of top-tiers in the range of modifications covered by the bin.
+	
+For example:  
+=> bin 10 covers modifications 5-15 and t_i is the number of top-tiers after i modifications  
+=> so the height of the bin is: mean(t_5 + t_6 + ... + t_15)  
+=> and the bin is located over x-position mean(5 + 6 + ... + 14 + 15) = 10
+
+Returns the finalized bar-plot (see also `finalizePlot!`).
 """
 function topTierOverTime(
 	agentDF::DataFrame,
@@ -97,23 +139,38 @@ function topTierOverTime(
 	algorithm::GeneticAlgorithm;
 	maxBins::Integer = 100
 )
+	# Find the maximum number of steps and modifications:
 	steps = maximum(agentDF[!, :step])
 	mods = maximum(agentDF[!, :modifications])
+
+	# Calculate the number of bins to disply in the plot:
 	bins = steps < maxBins ? steps : maxBins
+
+	# Define a function to determine the number of top-tiers in an array of scores:
 	topTiers = scores -> sum(scores .< (minimum(scores) + minimum(scores) * (percentage/100))) 
 	
+	# Group the data per number of modification:
 	topTierDF = groupby(agentDF, :modifications)
+
+	# Calculate the number of top-tiers for each number of modifications:
 	topTierDF = combine(topTierDF, :score => topTiers => :topTiers)
+
+	# Distribute the observations equally over <<bins>> classes:
 	topTierDF = transform(
 		topTierDF, :modifications => ByRow(m -> div(m - 1, div(mods, bins))) => :class
 	)
+
+	# Group the data by the previously defined classes:
 	topTierDF = groupby(topTierDF, :class)
+
+	# Calculate the mean top-tiers and mean modifications per class:
 	topTierDF = combine(
 		topTierDF, 
 		:topTiers => mean => :meanTopTiers, 
 		:modifications => mean => :meanMods
 	)
 
+	# Create the bar-plot:
 	plt = Plots.bar(
 		topTierDF[:, :meanMods],
 		topTierDF[:, :meanTopTiers],
@@ -124,5 +181,6 @@ function topTierOverTime(
 		bar_width = mods/(2*bins)
 	)
 
+	# Return the finalized plot:
 	return finalizePlot!(plt)
 end
