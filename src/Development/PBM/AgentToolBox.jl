d1a0af04a2963055721b2b92960a6d458df1c068
nick.diercksen <nick.diercksen@student.hswt.de>
2022-06-27 02:39:46 +0200

    added rotate_2dvector to toolbox

--- src/Development/PBM/AgentToolBox.jl
+++ src/Development/PBM/AgentToolBox.jl
@@ -4,7 +4,9 @@ This module can be used as a collection of useful functions for modelling
 """
 module AgentToolBox
 
-export getAgentsByType
+export getAgentsByType, rotate_2dvector
+
+DEGREES = 0:.01:2π
 
 """
     getAgentsByType(model, type)
@@ -13,5 +15,37 @@ returns all agents of the given type
 """
 getAgentsByType(model, type) = [agent for agent in allagents(model) if agent.type == type]
 
+
+"""
+    rotate_2dvector(φ, vector)
+
+rotates a given `vector` by a radial degree of φ
+"""
+function rotate_2dvector(φ, vector)
+    Tuple(
+        [
+            cos(φ) -sin(φ)
+            sin(φ) cos(φ)
+        ] *
+        [vector...]
+    )
+end
+
+"""
+    rotate_2dvector(φ, vector)
+
+rotates a given `vector` by a random degree ∈ -π:.01:π
+"""
+function rotate_2dvector(vector)
+    # more efficient to call `rand` on a variable (no need for additional allocations)
+    φ = rand(DEGREES)
+    Tuple(
+        [
+            cos(φ) -sin(φ)
+            sin(φ) cos(φ)
+        ] *
+        [vector...]
+    )
+end
 
 end
