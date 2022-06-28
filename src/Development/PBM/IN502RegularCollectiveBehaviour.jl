1818f46cbd9e20c1fccbd2913ce254c98750c6b6
Stefan <stefan-hausner96@hotmail.de>
2022-06-28 19:40:29 +0200

    IN502 vereinfachung

--- src/Development/PBM/IN502RegularCollectiveBehaviour.jl
+++ src/Development/PBM/IN502RegularCollectiveBehaviour.jl
@@ -1,17 +1,15 @@
-
 module CollectiveBehaviour
 using Agents
-using InteractiveDynamics, GLMakie,LinearAlgebra, Random, Statistics
+using InteractiveDynamics, GLMakie, Random, Statistics
 export demo
+#=
 mutable struct Particle <: AbstractAgent
     id::Int                    # Boid identity
     pos::NTuple{2,Float64}              # Position of boid
     vel::NTuple{2,Float64}              # Velocity direction of boid
-    speed::Float64                      # Speed of boid
-    rot::Float64
-    size:: Float64
-    mdist:: NTuple{2,Float64}  
 end
+=#
+ContinuousAgent{2}
 
 function rotate_2dvector(rotation,vector)
 
@@ -22,107 +20,96 @@ function rotate_2dvector(rotation,vector)
     end
 end
 
-function initialize_model(  ;n_particles = 10,
-                            globaldist = zeros(n_particles,2),
-                            tick=0,
-                            meadist=0,
-                            worldsize,
-                            psize,griddims = (worldsize, worldsize),
-                            particle_speed,
-                            movmentstate = zeros(n_particles,1))
+function einvec(vector)
+    vector1 = vector[1]/sqrt((vector[1])^2+(vector[2])^2)
+    vector2 = vector[2]/sqrt((vector[1])^2+(vector[2])^2)
+    vector = Tuple([vector1, vector2])
+end
+function initialize_model(  ;n_particles::Int = 50,
+                            meadist::Float64=0.0,
+                            worldsize::Int64,
+                            particlespeed::Float64,
+                            globaldist = zeros(n_particles,1),
+                            griddims = (worldsize, worldsize),
+                            )
 space2d = ContinuousSpace(griddims, 1.0)
 
 properties = Dict(
     :globaldist => globaldist,
-    :tick => tick,
     :meadist => meadist,
-    :movmentstate => movmentstate,
-    :worldsize => worldsize
+    :particlespeed => particlespeed,
+    :worldsize => worldsize,
 )
 
 
-model = ABM(Particle,space2d, scheduler = Schedulers.randomly,properties = properties)
-dist = zeros(n_particles,2)
+model = ABM(ContinuousAgent,space2d, scheduler = Schedulers.fastest,properties = properties)
 counter = 1
 for _ in 1:n_particles
-    size = psize
-    speed  = particle_speed;
-    π = 3.1415926535897
-    rot = rand(0:0.1:2π)
-    vel = rotate_2dvector(rot,[10 10])
+    vel = rotate_2dvector(rand(0:0.1:2π),[10 10])
     pos = Tuple([worldsize/2 worldsize/2]').+vel
-    vel = rotate_2dvector(0.5*π,[vel[1] vel[2]])
-    dist[counter,:] = [pos[1] pos[2]]
-    mdist = pos
+    vel = einvec(rotate_2dvector(0.5*π,[vel[1] vel[2]]))
+    model.globaldist[counter,1] = edistance(pos,Tuple([worldsize/2, worldsize/2]),model)
     add_agent!(
         pos,
         model,
         vel,
-        speed,
-        rot,
-        size,
-        mdist,
     )
     counter += 1
 end
-    model.globaldist = dist
-    model.meadist = mean(dist,dims=1)[1];
+    
     return model
 end
 
-function particle_marker(b::Particle;)
-    particle_polygon = Polygon(b.size*Point2f[(-0.25, -0.25), (0.5, 0), (-0.25, 0.25)])
-    φ = atan(b.vel[2], b.vel[1])
+function particle_marker(p::ContinuousAgent;)
+    particle_polygon = Polygon(Point2f[(-0.25, -0.25), (0.5, 0), (-0.25, 0.25)])
+    φ = atan(p.vel[2], p.vel[1])
     scale(rotate2D(particle_polygon, φ), 2)
 end
 
+function randomcolor(number)
+    
+
+    if (number == 1)
+        ac = :red
+    elseif (number == 2)
+        ac = :green
+    else
+        ac = :blue
+    end
+end
 function agent_step!(particle,model)
-    π = 3.1415926535897
     if rand()<0.1
-        particle.rot = rand()*((1/36)*π);
-        particle.vel= rotate_2dvector(particle.rot,particle.vel)
-        move_agent!(particle,model,particle.speed);
-        dist = particle.pos .- Tuple([(model.worldsize)/2 (model.worldsize)/2])
-        model.globaldist[particle.id,:] = [dist[1] dist[2]]' #ändern
-        model.movmentstate[particle.id,:] .= 1
-        #println(model.movmentstate)
-        #println("")
+        particle.vel= einvec(rotate_2dvector(rand()*((1/36)*π),particle.vel))
+        move_agent!(particle,model,model.particlespeed);
+        model.globaldist[particle.id,1] = edistance(particle.pos,Tuple([model.worldsize/2, model.worldsize/2]),model) #ändern
+        model.meadist = mean(model.globaldist,dims=1)[1]
+
     end
     
 end
 
-function model_step!(model)
-    if all(>(0),model.movmentstate)
-        model.tick += 1
-        model.meadist = sqrt((mean(model.globaldist,dims=1)[1])^2+(mean(model.globaldist,dims=1)[2])^2);
-        println(model.meadist)
-        model.movmentstate = zeros(10,1)
-        
-    end
-end
 
 function demo(world_size,particle_size,particle_speed)
-    model = initialize_model(worldsize = world_size,psize=particle_size,particle_speed=particle_speed);
+    model = initialize_model(worldsize = world_size,particlespeed=particle_speed);
 
-    println(model.meadist)
-    mdata = [:meadist,:tick]
-
-    figure,p= abmexploration(model;model_step!,agent_step!,params = Dict(),am = particle_marker,mdata)#,plotkwargs...);
+    mdata = [:meadist]
+    figure,p= abmexploration(model;agent_step!,params = Dict(),as=particle_size,am = particle_marker,mdata)#,plotkwargs...);
+    
+    
+    #=
     plot_layout = figure[:,end+1]
     count_layout = plot_layout[1,1] = GridLayout()
     ax_counts = Axis(count_layout[1,1];
     backgroundcolor = :white, ylabel = "Meandist")
 
-    mea = @lift(Point2f.($(p.mdf).tick, $(p.mdf).meadist))
-    #println(agent_steps[1])
+    mea = @lift(Point2f.($(p.mdf).step, $(p.mdf).meadist))
     #https://makie.juliaplots.org/stable/examples/plotting_functions/scatterlines/
     scatterlines!(ax_counts,mea)
     
     on(p.model) do m
-        #mea = @lift(Point2f.($(p.mdf).step, $(p.mdf).meadist))
-        #scatterlines!(ax_counts,mea)
         autolimits!(ax_counts)
-    end   
+    end
+    =#
     figure;
     
 end
