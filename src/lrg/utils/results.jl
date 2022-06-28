e9b740edd38c04db3d728091128fefdd968fb69a
Michael Staab <michael.staab@student.hswt.de>
2022-06-28 17:39:47 +0200

    comment code in results.jl properly

--- src/lrg/utils/results.jl
+++ src/lrg/utils/results.jl
@@ -1,8 +1,18 @@
-struct GASimulation
-    timestamp::DateTime
-    algorithm::GeneticAlgorithm
-    agentDF::DataFrame
+# =========================================================================================
+### results.jl: Defines structs to store the results of run simulations
+# =========================================================================================
 
+"""
+	GASimulation
+
+A GASimulation contains the results of a single simulation, which is run by calling `simulate`.
+"""
+struct GASimulation
+    timestamp::DateTime						# The end-time of the simulation
+    algorithm::GeneticAlgorithm				# The algorithm used for the simulation
+    agentDF::DataFrame						# The agent-data collected during the simulation
+
+	# Constructor of a GASimulation, that inserts the current time as simulation end-time:
     function GASimulation(
         algorithm::GeneticAlgorithm, 
         agentDF::DataFrame
@@ -11,11 +21,20 @@ struct GASimulation
     end
 end
 
-struct GAComparison
-    timestamp::DateTime
-    simulations::Vector{GASimulation}
-    runtimes::DataFrame
+# -----------------------------------------------------------------------------------------
+"""
+	GAComparison
 
+A GAComparison contains the results multiple genetic algorithm simulations to compare them afterwards.
+
+The vector `simulations` contains an element of `GASimulation` for every run simulation and `timestamp` is the end-time of the comparison. 
+"""
+struct GAComparison
+    timestamp::DateTime						# The end-time of the comparison
+    simulations::Vector{GASimulation}		# Compared simulations
+    runtimes::DataFrame						# The tracked simulation runtimes
+
+	# Constructor of a GAComparison, that inserts the current time as comparison end-time:
     function GAComparison(
         simulations::Vector{GASimulation},
         runtimes::DataFrame
