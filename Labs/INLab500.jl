646ea3e1d362cae5cf1b696c4d2b8aa9b76da2d2
nick.diercksen <nick.diercksen@student.hswt.de>
2022-07-02 00:35:16 +0200

    outlined introtuctory activities for ABMs

--- Labs/INLab500.jl
+++ Labs/INLab500.jl
@@ -3,12 +3,141 @@
 #
 # Welcome to course 500: An Introduction to Agent-Based Systems!
 #
-# Author: Niall Palfreyman, 24/04/2022
+# Author: Niall Palfreyman (24/04/2022), Nick Diercksen (July 2022)
 #========================================================================================#
 [
 	Activity(
 		"""
-		???
+		"Agent-based modelling (ABM) is a technique for understanding how the behaviour
+		of a complex biological system arises from the traits and behaviours of the
+		component agents that make up that system."
+		
+		...
+
+		# TODO: proper introduction to this topic
+		""",
+		"???",
+		x -> true
+	),
+	Activity(
+		"""
+		To use Agent Based Models in Julia, there is already a package available called
+			
+			Agents.jl
+
+		on which this course relies on.
+		We will be covering some of the basics to use this package, but if you find
+		yourself interested, I recommend checking out the official tuorial:
+			https://juliadynamics.github.io/Agents.jl/stable/tutorial/
+		
+		This package should already be in your environment, but if not you can add
+		it via the package manager.
+		As always, you will start by loading this package into your environment:
+
+			using Agents
+		
+		When done, go to the next activity ...
+		""",
+		"package not installed? Open the package manager with `]`, then type `add Agents`",
+		x -> true
+	),
+	Activity(
+		"""
+		# TODO: creating an agent
+		""",
+		"???",
+		x -> true
+	),
+	Activity(
+		"""
+		# TODO: creating/choosing a space
+		""",
+		"???",
+		x -> true
+	),
+	Activity(
+		"""
+		# TODO: creating an ABM
+		""",
+		"???",
+		x -> true
+	),
+	Activity(
+		"""
+		# TODO: Evolving the model (agent_step)
+		""",
+		"???",
+		x -> true
+	),
+	Activity(
+		"""
+		# TODO: Evolving the model (model_step)
+		""",
+		"???",
+		x -> true
+	),
+	Activity(
+		"""
+		# TODO: Evolving the model (dummystep)
+		""",
+		"???",
+		x -> true
+	),
+	Activity(
+		"""
+		# TODO: visualizing (abmplot)
+		""",
+		"???",
+		x -> true
+	),
+	Activity(
+		"""
+		# TODO: visualizing (remember Observables? a sophisticated framework we will use leverages those)
+		""",
+		"???",
+		x -> true
+	),
+	Activity(
+		"""
+		# TODO: visualizing (abmexploration [InteractiveDynamics])
+		""",
+		"???",
+		x -> true
+	),
+	
+	Activity(
+		"""
+		
+		""",
+		"???",
+		x -> true
+	),
+	Activity(
+		"""
+		# TODO: visualizing (abmvideo? is intro even needed? does this occur somehere? maybe at the end to possibly generate/export a video)
+		""",
+		"???",
+		x -> true
+	),
+
+	# maybe other chapters:
+	Activity(
+		"""
+		# TODO: visualizing (agent inspection)
+		""",
+		"???",
+		x -> true
+	),
+	Activity(
+		"""
+		# TODO: visualizing (custom abmplot: lifting)
+		""",
+		"???",
+		x -> true
+	),
+	Activity(
+		"""
+		# TODO: visualizing (mdata, adata)
 		""",
 		"???",
 		x -> true
