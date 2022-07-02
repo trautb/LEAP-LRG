126f7a50ceea0f03fc3aa9f6653902448bf95522
Stefan <stefan.hausner@student.hswt.de>
2022-07-02 20:12:17 +0200

    AgentToolbox Fix

--- src/Development/PBM/AgentToolBox.jl
+++ src/Development/PBM/AgentToolBox.jl
@@ -131,15 +131,30 @@ function wrapMat(size_row,size_col, index::Union{Vector{Vector{Int64}},Vector{In
       return  indeces
 end
 
+function diffuse4(mat::Matrix{Float64},rDiff::Float64)
+    size_row = size(mat)[1]
+    size_col = size(mat)[2]
+    map(CartesianIndices(( 1:size(mat)[1], 1:size(mat)[2]))) do x
+  
+      cart(i,j) = (j-1)*size_col+i
+      nmacart(i,j) = [cart(i+1,j), cart(i-1,j), cart(i,j-1), cart(i,j+1)]
+  
+      nma(i,j) = [[i+1,j], [i-1,j], [i,j-1], [i,j+1]]
+      if (x[1] == 1 || x[1] == size_row || x[2]== 1 || x[2]== size_col)
+          neighbours = wrapMat(size_row,size_col,nma(x[1],x[2]))
+      else
+          neighbours = nmacart(x[1],x[2])
+      end
+      flow = mat[x[1],x[2]]*rDiff
+      mat[x[1],x[2]] *= 1-rDiff
+      mat[neighbours] = mat[neighbours] .+ (flow/4)
+  
+    end
+    return mat
+  end
 
 
 function meanNb(mat::Matrix{Float64}, index::Vector{Vector{Int64}})
-        index1 = rem(index[ids][1] + size_row, size_row)
-        index2 = rem(index[ids][2] + size_col, size_col)
-        append!(indeces, [cart(index1, index2)])
-
-
-    end
 
     cart(i,j) = (j-1)*size_col+i
     sumup = []
