b46de3bb63d3539784e2ac55a4ae77fef972c9bf
nick.diercksen <nick.diercksen@student.hswt.de>
2022-07-10 01:33:41 +0200

    reinit DAH and converted spaced to tabs

--- src/Development/PBM/IN508DAH.jl
+++ src/Development/PBM/IN508DAH.jl
@@ -1,71 +1,74 @@
 module DAH
-export Cell3, demo2
+export Cell3, demo
 
 using InteractiveDynamics
 using GLMakie
 using Agents, LinearAlgebra
 using Random # hides
 include("./AgentToolBox.jl")
-import .AgentToolBox: rotate_2dvector, turn_left, turn_right
+import .AgentToolBox: rotate_2dvector, turn_left, turn_right, reinit_model_on_reset!
 
 mutable struct Cell3 <: AbstractAgent
-    # define the agent's properties
-    id::Int
-    pos::NTuple{2,Float64}
-    vel::NTuple{2,Float64}
-    halfstep::NTuple{2,Float64}
-    speed::Float64
-    cadherin::Float64
-    color::Symbol
+	# define the agent's properties
+	id::Int
+	pos::NTuple{2,Float64}
+	vel::NTuple{2,Float64}
+	halfstep::NTuple{2,Float64}
+	speed::Float64
+	cadherin::Float64
+	color::Symbol
 end
 
 
 function initialize_model(;
-    #give the function most of both the agent's and the model's own properties 
-    pPop =              1.0,
-    rAdhesionRange =    4.0,
-    rAdhesion =         0.4,
-    rRadius =           1.0,
-    rRepulsion =        -0.5,
-    rThermal =          0.01,
-    rGravity =          0.0,
-    nAdherins  =        3,
-    extent =            (40, 40)
-    )   
-    #properties of the model
-    properties = Dict(
-                    :pPop => pPop,
-                    :rAdhesionRange => rAdhesionRange,
-                    :rAdhesion => rAdhesion,
-                    :rRadius =>  rRadius,
-                    :rRepulsion => rRepulsion,
-                    :rThermal => rThermal,
-                    :rGravity => rGravity,
-                    :nAdherins =>  nAdherins,
-                    )
-    # Initialise Cell population
-    space2d = ContinuousSpace(extent)
-    model = ABM(Cell3, space2d; properties, scheduler = Schedulers.randomly)
-    vel = Tuple((0,0))
-    speed = 0
-    map(CartesianIndices(( 1:(extent[1]-1), 1:(extent[2]-1)))) do x
-        #
-        cadherin = rand(1:1:3)
-        if cadherin == 1 color=:darkorchid
-        elseif cadherin == 2 color=:cyan3
-        else color = :peru
-        end
-        add_agent!(
-            Tuple(x),
-            model,
-            vel,
-            ((0.0, 0.0)),
-            speed,
-            cadherin, 
-            color,
-        )
-    end 
-    return model
+	#give the function most of both the agent's and the model's own properties 
+	pPop=1.0,
+	rAdhesionRange=4.0,
+	rAdhesion=0.4,
+	rRadius=1.0,
+	rRepulsion=-0.5,
+	rThermal=0.01,
+	rGravity=0.0,
+	nAdherins=3,
+	extent=(40, 40)
+)
+	#properties of the model
+	properties = Dict(
+		:pPop => pPop,
+		:rAdhesionRange => rAdhesionRange,
+		:rAdhesion => rAdhesion,
+		:rRadius => rRadius,
+		:rRepulsion => rRepulsion,
+		:rThermal => rThermal,
+		:rGravity => rGravity,
+		:nAdherins => nAdherins,
+	)
+	# Initialise Cell population
+	space2d = ContinuousSpace(extent)
+	model = ABM(Cell3, space2d; properties, scheduler=Schedulers.randomly)
+	vel = Tuple((0, 0))
+	speed = 0
+	map(CartesianIndices((1:(extent[1]-1), 1:(extent[2]-1)))) do x
+		#
+		cadherin = rand(1:1:3)
+		if cadherin == 1
+			color = :darkorchid
+		elseif cadherin == 2
+			color = :cyan3
+		else
+			color = :peru
+		end
+		add_agent!(
+			Tuple(x),
+			model,
+			vel,
+			((0.0, 0.0)),
+			speed,
+			cadherin,
+			color,
+		)
+	end
+	return model
 end
 
 
@@ -74,60 +77,61 @@ end
 
 #function agent_step!(cell2,model)
 function model_step!(model::ABM)
-    
-    for i in model.agents
 
-        adhere(i[2],model)
-        repel(i[2],model)
-        meander(i[2],model)
+	for i in model.agents
 
-        move_agent!(i[2], i[2].halfstep, model)
-    end
+		adhere(i[2], model)
+		repel(i[2], model)
+		meander(i[2], model)
+
+		move_agent!(i[2], i[2].halfstep, model)
+	end
 end
 
 
-function  adhere(cell2::AbstractAgent,model::ABM)
+function adhere(cell2::AbstractAgent, model::ABM)
 
-    nbr = random_nearby_agent(cell2, model, model.rAdhesionRange)
-    if nbr !== nothing
-        #continue here
-        cell2.vel = get_direction(cell2.pos, nbr.pos, model)
-        cell2.speed = model.rAdhesion * cadhesion(cell2.cadherin, nbr.cadherin, model)
-        cell2.halfstep = Tuple(collect(cell2.pos)+collect(cell2.vel)*cell2.speed)
-    end       
+	nbr = random_nearby_agent(cell2, model, model.rAdhesionRange)
+	if nbr !== nothing
+		#continue here
+		cell2.vel = get_direction(cell2.pos, nbr.pos, model)
+		cell2.speed = model.rAdhesion * cadhesion(cell2.cadherin, nbr.cadherin, model)
+		cell2.halfstep = Tuple(collect(cell2.pos) + collect(cell2.vel) * cell2.speed)
+	end
 end
 
 
-function  repel(cell2::AbstractAgent,model::ABM)
-    penetrator = random_nearby_agent(cell2.halfstep, model, model.rRadius)
-    if penetrator !== nothing
-        #continue here
-        cell2.vel = get_direction(cell2.halfstep, penetrator.pos, model)
-        cell2.speed = model.rRepulsion 
-        cell2.halfstep= Tuple(collect(cell2.halfstep)+collect(cell2.vel)*cell2.speed)
-    end       
+function repel(cell2::AbstractAgent, model::ABM)
+	penetrator = random_nearby_agent(cell2.halfstep, model, model.rRadius)
+	if penetrator !== nothing
+		#continue here
+		cell2.vel = get_direction(cell2.halfstep, penetrator.pos, model)
+		cell2.speed = model.rRepulsion
+		cell2.halfstep = Tuple(collect(cell2.halfstep) + collect(cell2.vel) * cell2.speed)
+	end
 end
 
 
-function  meander(cell2::AbstractAgent,model::ABM)
-    
-    cell2.vel = turn_right(cell2,rand(1:360)) 
-    cell2.speed = model.rThermal
-    cell2.halfstep= Tuple(collect(cell2.halfstep)+collect(cell2.vel)*cell2.speed)
+function meander(cell2::AbstractAgent, model::ABM)
+
+	cell2.vel = turn_right(cell2, rand(1:360))
+	cell2.speed = model.rThermal
+	cell2.halfstep = Tuple(collect(cell2.halfstep) + collect(cell2.vel) * cell2.speed)
 end
 
 
 function cadhesion(c1::Float64, c2::Float64, model::ABM)
-    return(1-(2*abs(c1-c2) + c1 + c2) / (2*model.nAdherins))
+	return (1 - (2 * abs(c1 - c2) + c1 + c2) / (2 * model.nAdherins))
 end
 
 
-function demo2()
-    
-    model = initialize_model()
-    #create the interactive plot with our sliders
-    cellcolor(a::Cell3) = a.color
-    fig, p = abmexploration(model; model_step!, ac = cellcolor, as = 15)
-    fig
+function demo()
+
+	model = initialize_model()
+	#create the interactive plot with our sliders
+	cellcolor(a::Cell3) = a.color
+	fig, p = abmexploration(model; model_step!, ac=cellcolor, as=15)
+	reinit_model_on_reset!(p, fig, initialize_model)
+	fig
 end
 end
