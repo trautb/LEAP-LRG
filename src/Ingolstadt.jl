da56493c04693976311d93677a0be087e9a631c4
Niall Palfreyman <Niall.Palfreyman@hswt.de>
2022-09-14 18:38:12 +0200

    Renamed Ingolstadt.help() to menu()

--- src/Ingolstadt.jl
+++ src/Ingolstadt.jl
@@ -13,7 +13,7 @@ Author: Niall Palfreyman, 7/12/2021
 module Ingolstadt
 
 # Externally callable methods of Ingolstadt
-export gimme, lab, act, reply, hint, nextlab, nextact
+export gimme, lab, act, reply, hint, menu, nextlab, nextact
 
 using Pluto								# We want to be able to use Pluto notebooks
 
@@ -101,11 +101,11 @@ end
 
 #-----------------------------------------------------------------------------------------
 """
-help()
+menu()
 
-Display a list of Ingolstadt commands.
+Display a menu of Ingolstadt commands.
 """
-function help()
+function menu()
 	println( "List of Ingolstadt commands:")
 	println( "   hint()               : Display a hint for the current activity")
 	println( "   gimme()              : Give me the current activity")
@@ -270,7 +270,7 @@ function nextlab( lab_num::Int = 0, current_act::Int = 1)
 	println( "Great - I've set up the laboratory. If you've just completed working on another")
 	println( "laboratory, I recommend that you restart Julia now. This will keep your environment")
 	println( "clean and avoid naming conflicts.")
-	println( "Enter Ingolstadt.help() at any time to see your available options. Have fun! :)")
+	println( "Enter menu() at any time to see your available options. Have fun! :)")
 	println()
 
 	# Display the new laboratory number:
